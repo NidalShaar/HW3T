c71edee3436f4ddadc7a83c8a9420aa72685e0e7
Batma <Batma@DESKTOP-MN0GKBG>
2019-02-20 19:26:43 +0200

    HW3

---
+++ HW3T/src/HW3T.java
@@ -0,0 +1,26 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class HW3T {
+	ArrayList <Integer> count = new ArrayList <Integer>();
+	    ArrayList <Integer> price = new ArrayList <Integer>();
+	    
+	    public HW3T(){
+	        
+	    }
+	  public void func(int c){
+	   count.add(c);
+	  
+	  } 
+	    public void func2(int p){
+	   price.add(p);
+	  
+	  } 
+	  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HW3T/src/HW3Testcases.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HW3Testcases {
+    @Test
+	public void count() {
+    HW3T c=new HW3T();
+    int z =0;
+    int y=0;
+        for (int i=0;i<4;i++){
+             c.func(1);
+             c.func2(50);
+        }
+    for(int i=0;i<c.count.size();i++){
+        z=z+c.count.get(i);
+        y=y+c.price.get(i);
+    }
+    assertEquals(4,z);
+    assertEquals(200, y);
+    }
+	 
+	 
+	 @Test
+	    public void count2() {
+		    HW3T c=new HW3T();
+	    int z =0;
+	    
+	    for(int i=0;i<c.count.size();i++){
+	        z=z+c.count.get(i);
+	       
+	    }
+	    assertEquals(0,z);
+	    }
+	 
+	 
+	 
+	
+	 
+	
+}
+
